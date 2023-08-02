db20104ef0b183e23fafede129299243b363de9a
prateekchhibber <0392pc@gmail.com>
2023-08-02 13:40:52 +0530

    Build Get ALL Employees REST API

--- src/main/java/com/ernest/emsbackend/service/EmployeeService.java
+++ src/main/java/com/ernest/emsbackend/service/EmployeeService.java
@@ -2,9 +2,13 @@ package com.ernest.emsbackend.service;
 
 import com.ernest.emsbackend.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
